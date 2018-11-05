c9156cb2f7dcea0d6a4490f5505a535c93f9f4e6
Scott Sawyer <38329255+scottasawyer@users.noreply.github.com>
2018-11-05 16:53:19 -0500

    Updates

---
+++ precache-manifest.f8cefcfa9b2aeb8c299c5c33d047879c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb7902abb711ddb46128",
+    "url": "/Click-It/static/js/runtime~main.bb7902ab.js"
+  },
+  {
+    "revision": "0de7fdce758e2c623749",
+    "url": "/Click-It/static/js/main.0de7fdce.chunk.js"
+  },
+  {
+    "revision": "f843a591dc3acd9073f0",
+    "url": "/Click-It/static/js/1.f843a591.chunk.js"
+  },
+  {
+    "revision": "30e37e158383ecbb4089404cc1da638f",
+    "url": "/Click-It/index.html"
+  }
+];
